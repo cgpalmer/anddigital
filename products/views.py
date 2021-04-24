38d628bdcd6f0d6ccc1cbd2347f86790af4fb1ac
Chris Palmer <cgpalmer91@gmail.com>
2021-04-24 15:33:46 +0000

    Creating a path to the individual products.

--- products/views.py
+++ products/views.py
@@ -40,3 +40,12 @@ def add_product(request):
         'form': form,
     }
     return render(request, template, context)
+
+
+def product_detail(request, product_id):
+    # Returning the details of a specific product.
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
