baf7acc72eb4127d7670e1ce1ba64705d49571b9
Chris Palmer <cgpalmer91@gmail.com>
2021-04-24 12:04:29 +0000

    Putting in the static root.

--- golden_shoe/settings.py
+++ golden_shoe/settings.py
@@ -156,8 +156,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
