35aa574a4d919718e54aa2340250605c91527861
Chris Palmer <cgpalmer91@gmail.com>
2021-04-25 09:14:32 +0000

    First attempt at sending auto response to customer enquiry.

--- customer_service/views.py
+++ customer_service/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def customer_enquiry(request):
-    
+    if form.is_valid():
+        customer_enquiry = form.cleaned_data.get("enquiry")
+        email = form.cleaned_data.get("email")
+        tel = form.cleaned_data.get("tel")
+        send_confirmation_email(self, customer_enquiry, email, tel)
     template = 'home/index.html'
     context = {        
     }
@@ -18,15 +22,16 @@ def delivery_enquiry(request):
     return render(request, template, context)
 
 
-def send_confirmation_email(self, order):
-    cust_email = order.email
+def send_confirmation_email(self, customer_enquiry, email, tel):
+    cust_email = email
     """Send the user a confirmation email"""
     subject = render_to_string(
-        'checkout/confirmation_emails/confirmation_email_subject.txt',
-        {'order': order})
+        'auto_query_response_subject.txt',
+        )
     body = render_to_string(
-        'checkout/confirmation_emails/confirmation_email_body.txt',
-        {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        'customer_service/auto_response_to_query.txt',
+        {'enquiry': customer_enquiry}
+        )
 
     send_mail(
         subject,
