75a4d432b444a652a33e7fb094680adfe894b1f2
Chris Palmer <cgpalmer91@gmail.com>
2021-04-25 11:02:32 +0000

    Adding in an email to the store email address.

--- customer_service/views.py
+++ customer_service/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+import datetime
 
 # Create your views here.
 def customer_enquiry(request):
  
-    customer_enquiry = form.cleaned_data.get("enquiry")
-    email = form.cleaned_data.get("email")
-    tel = form.cleaned_data.get("tel")
-    send_confirmation_email(self, customer_enquiry, email, tel)
+    customer_enquiry = request.POST.get("enquiry")
+    print(customer_enquiry)
+    email =  request.POST.get("email")
+    tel =  request.POST.get("tel")
+    send_query_confirmation_email(customer_enquiry, email, tel)
+    send_store_query_email(customer_enquiry, email, tel)
     template = 'home/index.html'
     context = {        
     }
@@ -21,21 +27,44 @@ def delivery_enquiry(request):
 
     return render(request, template, context)
 
+# https://github.com/lornebb/NullAnIon/blob/master/checkout/webhook_handler.py
+def send_query_confirmation_email(customer_enquiry, email, tel):
+    if email:
+        cust_email = email
+        """Send the user a confirmation email"""
+        subject = render_to_string(
+            'customer_service/auto_query_response_subject.txt',
+            )
+        body = render_to_string(
+            'customer_service/auto_response_to_query.txt',
+            {'enquiry': customer_enquiry}
+            )
 
-def send_confirmation_email(self, customer_enquiry, email, tel):
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )
+
+def send_store_query_email(customer_enquiry, email, tel):
     cust_email = email
+    cust_tel = tel
+    store_email = 'cgpalmer91@gmail.com'
+    enquiry_date = datetime.datetime.now()
     """Send the user a confirmation email"""
     subject = render_to_string(
-        'auto_query_response_subject.txt',
+        'customer_service/store_enquiry_subject.txt',
+        {'enquiry_date': enquiry_date}
         )
     body = render_to_string(
-        'customer_service/auto_response_to_query.txt',
-        {'enquiry': customer_enquiry}
+        'customer_service/store_enquiry_body.txt',
+        {'enquiry': customer_enquiry, 'cust_email': cust_email, 'cust_tel': tel}
         )
 
     send_mail(
         subject,
         body,
         settings.DEFAULT_FROM_EMAIL,
-        [cust_email]
+        [store_email]
     )
